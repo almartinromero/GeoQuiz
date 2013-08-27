de23b9dfd7e6e2b4391c7469da4d030f86571587
dejadeses <dejadeses@gmail.com>
2013-08-27 20:07:42 +0200

    ImageButtons created successfully

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,7 +13,8 @@ public class QuizActivity extends Activity {
 
 	private Button mTrueButton;
 	private Button mFalseButton;
-	private Button mNextButton;
+	private ImageButton mNextButton;
+	private ImageButton mPrevButton;
 	private TextView mQuestionTextView;
 	
 	private TrueFalse[] mQuestionBank = new TrueFalse[]
@@ -78,13 +80,27 @@ public class QuizActivity extends Activity {
 			}
 		});
         
-        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+        
+        mPrevButton = (ImageButton)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(mCurrentIndex == 0)
+					mCurrentIndex = mQuestionBank.length - 1;
+				else
+					mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
 				updateQuestion();
 			}
 		});
